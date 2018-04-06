6aa476c03ad0c911e3f7ae3fe7170c935210f241
oaxelou <oaxelou@github.com>
2018-04-06 18:52:37 +0300

    UnsupportedFileFormatException added!

---
+++ Java/hw2/UnsupportedFileFormatException.java
@@ -0,0 +1,11 @@
+// package ce325.hw2;
+
+public class UnsupportedFileFormatException extends java.lang.Exception{
+  public UnsupportedFileFormatException(){
+    super();
+  }
+
+  public UnsupportedFileFormatException(String msg){
+    super(msg);
+  }
+}
